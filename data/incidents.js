78a684b7c570d9fc8c16df49d0630fa2cc29fd4f
darshLogicgo <darsh.logicgo@gmail.com>
2025-03-19 18:11:09 +0530

    Api integration

--- data/incidents.js
+++ data/incidents.js
@@ -1,95 +1,36 @@
 const incidents = [
   {
-    "message": "Event post fetched successfully.",
-    "body": {
-      "_id": "67ced6320ae49725f7780ab3",
-      "title": "Example Event",
-      "description": "This is an example description for an event with multiple media.This is an example description for an event with multiple media.This is an example description for an event with multiple media.This is an example description for an event with multiple media.This is an example description for an event with multiple media.This is an example description for an event with multiple media.",
-      "latitude": 22.5726,
-      "longitude": 88.3639,
-      "eventTime": "2025-03-20T15:30:00.000Z",
-      "viewCounts": "10",
-      "commentCounts": "2",
-      "reactionCounts": "5",
-      "sharedCount": "3",
-      "notifiedUserCount": "15",
-      "hashTags": [
-        "#example",
-        "#event"
-      ],
-      "lostItemName": null,
-      "countryCode": "IN",
-      "mobileNumber": "+919876543210",
-      "address": "Park Street, Kolkata",
-      "postCategory": "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/event-type/1739334564445.png",
-      "postType": "incident",
-      "reactionIcon": "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/event-reaction/1739424416698.png",
-      "attachments": [
-        {
-          "userId": null,
-          "eventTime": "2025-03-20T15:30:00.000Z",
-          "type": "default",
-          "title": "Image Attachment",
-          "attachment": "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/profile-picture/20250313062203.jpg",
-          "thumbnail": null,
-          "description": "An image from the event.",
-          "attachmentId": "20250320153001",
-          "name": null,
-          "profilePicture": null,
-          "isSensitiveContent": false,
-          "attachmentFileType": "Image",
-          "attachmentViewCounts": "5"
-        },
-        {
-          "userId": null,
-          "eventTime": "2025-03-20T15:30:00.000Z",
-          "type": "default",
-          "title": "Video Attachment",
-          "attachment": "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/event-post/1741860624993.mp4",
-          "thumbnail": "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/dev-event-post/1741608497915.jpg",
-          "description": "A video clip from the event.",
-          "attachmentId": "20250320153002",
-          "name": null,
-          "profilePicture": null,
-          "isSensitiveContent": false,
-          "attachmentFileType": "Video",
-          "attachmentViewCounts": "8"
-        },
-        {
-          "userId": null,
-          "eventTime": "2025-03-20T15:30:00.000Z",
-          "type": "default",
-          "title": "Audio Attachment",
-          "attachment": "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/event-type/1739334564445.png",
-          "thumbnail": null,
-          "description": "An audio clip from the event.",
-          "attachmentId": "20250320153003",
-          "name": null,
-          "profilePicture": null,
-          "isSensitiveContent": false,
-          "attachmentFileType": "Audio",
-          "attachmentViewCounts": "3"
-        },
-        {
-          "userId": null,
-          "eventTime": "2025-03-20T15:30:00.000Z",
-          "type": "default",
-          "title": "Audio Attachment",
-          "attachment": "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/profile-picture/20250313062203.jpg",
-          "thumbnail": null,
-          "description": "An audio clip from the event.",
-          "attachmentId": "20250320153003",
-          "name": null,
-          "profilePicture": null,
-          "isSensitiveContent": false,
-          "attachmentFileType": "Audio",
-          "attachmentViewCounts": "3"
-        }
-      ],
-      "timeLines": [],
-      "rescueUpdates": []
-    },
-    "status": true
+    id: "1",
+    title: "Fire Breakout in Downtown",
+    description: "A massive fire broke out in downtown. Firefighters are on the scene.",
+    media: [
+      "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/event-post/1741860624993.mp4",
+      "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/event-type/1739334564445.png",
+      "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/profile-picture/20250313062203.jpg",
+      "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/event-type/1739334564445.png",
+      "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/profile-picture/20250313062203.jpg",
+      "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/event-type/1739334564445.png",
+    ],
+    views: "28.9K",
+    distance: "3.8 km",
+    date: "27 Nov",
+    time: "12h",
+    notified: "13.5k"
+  },
+  {
+    id: "2",
+    title: "Flood Alert Issued",
+    description: "Heavy rains have led to floods in the city. Authorities have issued warnings.",
+    media: [
+      "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/event-type/1739334564445.png",
+      "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/profile-picture/20250313062203.jpg",
+      "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/event-type/1739334564445.png",
+    ],
+    views: "1.6M",
+    distance: "5.2 km",
+    date: "28 Nov",
+    time: "8h",
+    notified: "20.1k"
   }
 ];
 
