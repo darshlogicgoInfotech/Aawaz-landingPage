5d77b8e06dd5d2b4ad6fa51947a1c8548361474a
darshLogicgo <darsh.logicgo@gmail.com>
2025-03-20 14:39:40 +0530

    without design

--- data/incidents.js
+++ data/incidents.js
@@ -1,37 +1,37 @@
-const incidents = [
-  {
-    id: "67cfbc725cd45c3a15343fe4",
-    title: "Fire Breakout in Downtown",
-    description: "A massive fire broke out in downtown. Firefighters are on the scene.",
-    media: [
-      "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/event-post/1741667441653.mp4",
-      "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/event-type/1739334564445.png",
-      "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/profile-picture/20250313062203.jpg",
-      "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/event-type/1739334564445.png",
-      "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/profile-picture/20250313062203.jpg",
-      "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/event-type/1739334564445.png",
-    ],
-    views: "28.9K",
-    distance: "3.8 km",
-    date: "27 Nov",
-    time: "12h",
-    notified: "13.5k"
-  },
-  {
-    id: "67cfd4605cd45c3a153440cb",
-    title: "Flood Alert Issued",
-    description: "Heavy rains have led to floods in the city. Authorities have issued warnings.",
-    media: [
-      "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/event-post/1741671182192.mp4",
-      "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/profile-picture/20250313062203.jpg",
-      "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/event-type/1739334564445.png",
-    ],
-    views: "1.6M",
-    distance: "5.2 km",
-    date: "28 Nov",
-    time: "8h",
-    notified: "20.1k"
-  }
-];
+// const incidents = [
+//   {
+//     id: "67cfbc725cd45c3a15343fe4",
+//     title: "Fire Breakout in Downtown",
+//     description: "A massive fire broke out in downtown. Firefighters are on the scene.",
+//     media: [
+//       "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/event-post/1741667441653.mp4",
+//       "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/event-type/1739334564445.png",
+//       "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/profile-picture/20250313062203.jpg",
+//       "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/event-type/1739334564445.png",
+//       "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/profile-picture/20250313062203.jpg",
+//       "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/event-type/1739334564445.png",
+//     ],
+//     views: "28.9K",
+//     distance: "3.8 km",
+//     date: "27 Nov",
+//     time: "12h",
+//     notified: "13.5k"
+//   },
+//   {
+//     id: "67cfd4605cd45c3a153440cb",
+//     title: "Flood Alert Issued",
+//     description: "Heavy rains have led to floods in the city. Authorities have issued warnings.",
+//     media: [
+//       "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/event-post/1741671182192.mp4",
+//       "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/profile-picture/20250313062203.jpg",
+//       "https://guardianshot.blr1.cdn.digitaloceanspaces.com/eagleEye/event-type/1739334564445.png",
+//     ],
+//     views: "1.6M",
+//     distance: "5.2 km",
+//     date: "28 Nov",
+//     time: "8h",
+//     notified: "20.1k"
+//   }
+// ];
 
-export default incidents;
+// export default incidents;
